f32446bc5a7bf2240cf245f598ed94b6ff9f6c9e
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-07-24 18:20:42 -0300

    feat: Add custom colors to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
         lighterGray: '#D9D9D9',
         darkGray: '#898989',
         graphite: '#333333',
+        lighterGraphite: '#616161',
+        lightGraphite: '#666666',
+        darkGraphite: '#3A3A3A',
         cream: '#FAF3EA',
         lightYellow: '#FFF3E3',
         golden: '#B88E2F'
