5dff72c54b79d4eb2fe3ba91ed586d6a9541452c
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-07-26 14:01:53 -0300

    feat(pages): Add contact page with basic layout

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import TopBanner from '../components/TopBanner'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+
+const Contact = () => {
+  return (
+    <div>
+        <Header />
+        <TopBanner title='Contact' section='Contact' />
+        <Banner />
+        <Footer />
+    </div>
+  )
+}
+
+export default Contact
