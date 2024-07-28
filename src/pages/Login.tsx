371ea0fd92975dcaedb443ac43cc41686d5f1e0b
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-07-28 00:57:01 -0300

    feat(pages): Create login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import React from 'react';
+import Header from '../components/Header';
+import LoginForm from '../components/LoginForm';
+import cover from '../assets/login-cover.jpeg';
+import logo from '../assets/furniro-icon.svg';
 
-const Login = () => {
+const Login: React.FC = () => {
   return (
-    <div>
-        <Header />
-        <Footer />
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <div className='flex flex-1 w-full'>
+        <div
+          className='relative overflow-hidden flex items-center justify-center w-3/5'
+          style={{
+            backgroundImage: `linear-gradient(to right, rgba(250, 243, 234, 0.6) 40%, #faf3ea 100%), url(${cover})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backdropFilter: 'blur(10px)',
+          }}
+        >
+          <div className='relative z-10 text-black text-center'>
+            <img src={logo} className='w-[200px] mx-auto mb-4' />
+            <p className='font-poppins text-5xl font-bold'>Furniro</p>
+          </div>
+        </div>
+        <div className='w-2/5 flex items-center justify-center bg-cream'>
+          <div className='flex flex-col items-center p-6 max-h-full overflow-auto'>
+            <LoginForm />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
+
