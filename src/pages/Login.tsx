aed119aa2f4e78987f90026b041a9b93d20d2062
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-08-05 13:44:35 -0300

    fix: Adjust login page responsiveness for mobile devices

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header from '../components/Header';
-import LoginForm from '../components/LoginForm';
-import cover from '../assets/login-cover.jpeg';
-import logo from '../assets/furniro-icon.svg';
+import React from 'react'
+import Header from '../components/Header'
+import LoginForm from '../components/LoginForm'
+import cover from '../assets/login-cover.jpeg'
+import logo from '../assets/furniro-icon.svg'
 
 const Login: React.FC = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <div className='flex flex-1 w-full'>
+      <div className='flex flex-col md:flex-row flex-1 w-full'>
         <div
-          className='relative overflow-hidden flex items-center justify-center w-3/5'
+          className='hidden md:flex relative overflow-hidden items-center justify-center w-full md:w-3/5'
           style={{
             backgroundImage: `linear-gradient(to right, rgba(250, 243, 234, 0.6) 40%, #faf3ea 100%), url(${cover})`,
             backgroundSize: 'cover',
@@ -18,20 +18,19 @@ const Login: React.FC = () => {
             backdropFilter: 'blur(10px)',
           }}
         >
-          <div className='relative z-10 text-black text-center'>
-            <img src={logo} className='w-[200px] mx-auto mb-4' />
-            <p className='font-poppins text-5xl font-bold'>Furniro</p>
+          <div className='relative z-10 text-black text-center p-6'>
+            <img src={logo} className='w-[150px] md:w-[200px] mx-auto mb-4' />
+            <p className='font-poppins text-4xl md:text-5xl font-bold'>Furniro</p>
           </div>
         </div>
-        <div className='w-2/5 flex items-center justify-center bg-cream'>
+        <div className='w-full md:w-2/5 flex items-center justify-center bg-cream'>
           <div className='flex flex-col items-center p-6 max-h-full overflow-auto'>
             <LoginForm />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
-
+export default Login
