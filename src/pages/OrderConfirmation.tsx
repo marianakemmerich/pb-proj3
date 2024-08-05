d5d21aca73d2390dd17f0c389ba11a8e3b31cc11
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-08-05 14:10:12 -0300

    fix: Adjust confirmation page responsiveness for mobile devices

--- src/pages/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
-const OrderConfirmation = () => {
-  const navigate = useNavigate();
+const OrderConfirmation: React.FC = () => {
+  const navigate = useNavigate()
 
   const handleButtonClick = () => {
-    navigate('/');
+    navigate('/')
   };
 
   return (
     <div>
       <Header />
-      <div className='bg-cream w-full h-[650px] flex justify-center items-center'>
-        <div className='bg-white p-4 w-1/2 h-1/2 rounded font-poppins flex flex-col items-center justify-center'>
-          <h1 className='text-5xl text-golden font-bold mb-8'>Thank you for your purchase!</h1>
-          <p className='text-lg text-black text-center mb-8'>
+      <div className='bg-cream w-full h-screen flex justify-center items-center'>
+        <div className='bg-white p-4 md:p-8 w-4/5 md:w-1/2 h-auto md:h-1/2 rounded font-poppins flex flex-col items-center justify-center'>
+          <h1 className='text-3xl md:text-5xl text-center text-golden font-bold mb-4 md:mb-8'>Thank you for your purchase!</h1>
+          <p className='text-base md:text-lg text-black text-center mb-4 md:mb-8'>
             We are getting started with your order right away.<br /> 
             Your package will be sent within 48h.
           </p>
           <button 
-            className='w-1/5 h-[55px] bg-golden font-semibold text-white text-base'
+            className='w-3/4 md:w-1/5 h-[45px] md:h-[55px] bg-golden font-semibold text-white text-base md:text-base rounded'
             onClick={handleButtonClick}
           >
             Ok
@@ -30,7 +30,7 @@ const OrderConfirmation = () => {
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default OrderConfirmation;
+export default OrderConfirmation
