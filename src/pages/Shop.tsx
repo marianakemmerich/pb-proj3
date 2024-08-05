c13284d0462db96204eae41d5a3b01bd8b5f428b
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-08-05 11:04:44 -0300

    feat: Enhance responsiveness of Shop page

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -6,46 +6,48 @@ import { fetchProducts } from '../api/apiService'
 import { IProducts } from '../types/Products'
 import Banner from '../components/Banner'
 import Footer from '../components/Footer'
+import Filter from '../components/Filter'
 
 const Shop: React.FC = () => {
-        const [products, setProducts] = useState<IProducts[]>([])
-    
-        useEffect(() => {
-            const getProducts = async () => {
-            const data = await fetchProducts()
-            setProducts(data.slice(0, 16))
-            }
-    
-            getProducts()
-        }, [])
+  const [products, setProducts] = useState<IProducts[]>([])
+  const [filteredProducts, setFilteredProducts] = useState<IProducts[]>([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await fetchProducts()
+      setProducts(data)
+      setFilteredProducts(data.slice(0, 16))
+    }
+
+    getProducts()
+  }, [])
 
   return (
     <div>
-        <Header />
-        <TopBanner title='Shop' section='Shop' />
-        <div className='flex flex-col items-center w-full min-h-screen py-8'>
-                <div className='container mx-auto p-4'>
-                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8'>
-                        {products.map((product) => (
-                            <ProductCard
-                                key={product.id}
-                                product={product}
-                            />
-                        ))}
-                    </div>
-                </div>
-                <div className='font-poppins text-lg flex justify-center mt-8 mb-12 space-x-4'>
-                    <button className='bg-cream w-[50px] h-[50px] px-4 py-2 bg-gray-200 rounded'>1</button>
-                    <button className='bg-cream w-[50px] h-[50px] px-4 py-2 bg-gray-200 rounded'>2</button>
-                    <button className='bg-cream w-[50px] h-[50px] px-4 py-2 bg-gray-200 rounded'>3</button>
-                    <button className='bg-cream w-[88px] h-[50px] px-4 py-2 bg-gray-200 rounded'>Next</button>
-                </div>
-            </div>
-        <Banner />
-        <Footer />
+      <Header />
+      <TopBanner title='Shop' section='Shop' />
+      <div className='flex flex-col items-center w-full min-h-screen'>
+        <Filter products={products} onFilter={setFilteredProducts} />
+        <div className='container mx-auto p-4'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8'>
+            {filteredProducts.map((product) => (
+              <div className='flex justify-center' key={product.id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='font-poppins text-lg flex justify-center mt-8 mb-12 space-x-4'>
+          <button className='bg-cream w-[50px] h-[50px] px-4 py-2 bg-gray-200 rounded'>1</button>
+          <button className='bg-cream w-[50px] h-[50px] px-4 py-2 bg-gray-200 rounded'>2</button>
+          <button className='bg-cream w-[50px] h-[50px] px-4 py-2 bg-gray-200 rounded'>3</button>
+          <button className='bg-cream w-[88px] h-[50px] px-4 py-2 bg-gray-200 rounded'>Next</button>
+        </div>
+      </div>
+      <Banner />
+      <Footer />
     </div>
   )
 }
-
 
 export default Shop
