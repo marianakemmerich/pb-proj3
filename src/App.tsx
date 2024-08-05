afbd5e9e087a1363e52c93996ffd081420036f1c
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-08-05 15:34:57 -0300

    feat: Implement route protection for checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import OrderConfirmation from './pages/OrderConfirmation';
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import OrderConfirmation from './pages/OrderConfirmation'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App: React.FC = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/order-confirmation" element={<OrderConfirmation />} />
-      </Routes>
-    </>
-  );
-};
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<ProtectedRoute element={<Checkout />} />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/confirmation" element={<OrderConfirmation />} />
+    </Routes>
+  )
+}
 
-export default App;
+export default App
